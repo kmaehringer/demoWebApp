f9cd1a548aa11442547bfc87e00cd51a07128af2
Klaus Mähringer <kmaehringer@websel.de>
2017-04-03 23:00:58 +0200

    initial OAuth2Sso Client

--- src/main/java/de/websel/demo/app/DemoWebAppApplication.java
+++ src/main/java/de/websel/demo/app/DemoWebAppApplication.java
@@ -2,11 +2,13 @@ package de.websel.demo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class DemoWebAppApplication {
 
-	public static void main(String[] args) throws Throwable {
+	public static void main(String[] args) {
 		SpringApplication.run(DemoWebAppApplication.class, args);
 	}
 }
